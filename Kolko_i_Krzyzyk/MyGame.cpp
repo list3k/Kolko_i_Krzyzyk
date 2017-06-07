cb8ae9c8437041f4321d3a92ae698e9932fd4749
list3k <list3k@users.noreply.github.com>
2017-06-07 11:58:01 +0200

    more comments

--- Kolko_i_Krzyzyk/MyGame.cpp
+++ Kolko_i_Krzyzyk/MyGame.cpp
@@ -20,7 +20,7 @@ void MyGame::play()
 	//Main game loop
 	while (gameWindow.isOpen()) {
 		sf::Event event;
-		
+
 		while (gameWindow.pollEvent(event)) {
 			switch (event.type) {
 			case sf::Event::Closed:
@@ -28,7 +28,7 @@ void MyGame::play()
 				break;
 			}
 
-			//Check clicks
+			//Check click
 			if (!win && event.type == sf::Event::MouseButtonPressed) {
 				handleClick(gameBoard, turn, event.mouseButton.x, event.mouseButton.y);
 				win = isWinner(gameBoard, turn);
@@ -44,9 +44,9 @@ void MyGame::play()
 						turn = 1;
 					}
 				}
+
 			}
 		}
-
 		gameWindow.clear();
 
 		//draw main window with lines
@@ -57,12 +57,9 @@ void MyGame::play()
 			for (int j = 0; j < 3; ++j)
 				drawSlot(gameWindow, gameBoard[i][j], i, j);
 
-		// if some1 wins, draw the scene
 		if (win) {
 			drawWinScene(gameWindow, turn);
-			
 		}
-
 		gameWindow.display();
 	}
 }
@@ -139,276 +136,17 @@ void MyGame::drawWinScene(sf::RenderWindow &gameWindow, int side)
 	winText.setFont(winFont);
 	winText.setCharacterSize(50);
 	winText.setColor(sf::Color::Red);
-	winText.setStyle(sf::Text::Regular);
-	winText.setPosition(40.0, 250.0);
+	winText.setPosition(40, 250);
 
 	if (side == 1) {
 		winText.setString("Player O wins!");
-	}
-	else if (side == 2) {
-		winText.setString("Player X wins!");
-	}
-	gameWindow.draw(winText);	
-}
-
-bool MyGame::isWinner(std::vector<std::vector<int>> &gameBoard, int side)
-{
-	int count;
-
-	//check horizontally
-	for (int i = 0; i < 3; ++i)
-	{
-		count = 0;
-		for (int j = 0; j < 3; ++j)
-		{
-			if (gameBoard[i][j] == side)
-				++count;
-		}
-
-		if (count == 3)
-		{
-			return true;
-			
-		}
-	}
-
-	//check vertically
-	for (int j = 0; j < 3; ++j)
-	{
-		count = 0;
-		for (int i = 0; i < 3; ++i)
-		{
-			if (gameBoard[i][j] == side)
-				++count;
-		}
-
-		if (count == 3)
-		{
-			return true;
-		}
-	}
-
-	//check diagonally
-	count = 0;
-	for (int i = 0, j = 0; i < 3; ++i, ++j)
-	{
-		if (gameBoard[i][j] == side)
-			++count;
-	}
-
-	if (count == 3)
-	{
-		return true;
-	}
-
-	count = 0;
-	for (int i = 0, j = 2; i < 3; ++i, --j)
-	{
-		if (gameBoard[i][j] == side)
-			++count;
-	}
-
-	if (count == 3)
-	{
-		return true;
-	}
-
-	return false;
-
-}
-
-bool MyGame::handleClick(std::vector<std::vector<int>> &gameBoard, int side, int io, int jo)
-{
-	int i, j;
-
-	//searching for clicked element and getting is into table array - clicked middle = board[1][1]
-
-	//y for i
-	if (io < 200)
-	{
-		i = 0;
-	}
-	else if (io < 400)
-	{
-		i = 1;
-	}
-	else
-	{
-		i = 2;
-	}
-
-	//x for j
-	if (jo < 200)
-	{
-		j = 0;
-	}
-	else if (jo < 400)
-	{
-		j = 1;
-	}
-	else
-	{
-		j = 2;
-	}
-
-	if (gameBoard[i][j] == 0)   //if the slot is empty
-	{
-		gameBoard[i][j] = side;
-		return true;
-	}
-
-	return false;
-=======
-#include "MyGame.h"
-
-MyGame::MyGame() :
-
-	gameWindow(sf::VideoMode(600, 600, 32), "Kolko i Krzyzyk by Radek & Michal"),
-	gameBoard{
-		{ { 0,0,0 },
-		{   0,0,0 },
-		{   0,0,0 } }
-					}
-
-{
-}
-
-void MyGame::play()
-{
-	int turn = 1;
-	bool win = false;
-
-	//Main game loop
-	while (gameWindow.isOpen()) {
-		sf::Event event;
-
-		while (gameWindow.pollEvent(event)) {
-			switch (event.type) {
-			case sf::Event::Closed:
-				gameWindow.close();
-				break;
-			}
-
-			//Check click
-			if (!win && event.type == sf::Event::MouseButtonPressed) {
-				handleClick(gameBoard, turn, event.mouseButton.x, event.mouseButton.y);
-				win = isWinner(gameBoard, turn);
-
-				if (!win)
-				{
-					if (turn == 1)
-					{
-						turn = 2;
-					}
-					else
-					{
-						turn = 1;
-					}
-				}
-
-				if (win) {
-					drawWinScene(gameWindow, turn);
-				}
-
-
-			}
-		}
-		gameWindow.clear();
-
-		//draw main window with lines
-		drawBoard(gameWindow);
-
-		//draw the slots
-		for (int i = 0; i < 3; ++i)
-			for (int j = 0; j < 3; ++j)
-				drawSlot(gameWindow, gameBoard[i][j], i, j);
-
-		gameWindow.display();
-	}
-}
-
-void MyGame::drawBoard(sf::RenderWindow &gameWindow) 
-{
-	//clear the window to black
-	gameWindow.clear(sf::Color::Black);
-
-	//draw horizontal lines
-	sf::RectangleShape line1(sf::Vector2f(560, 2));
-	line1.setFillColor(sf::Color::White);
-	line1.setPosition(20, 200);
-
-	sf::RectangleShape line2 = line1;
-	line2.setPosition(20, 400);
-
-	//draw vertical lines;
-	sf::RectangleShape line3(sf::Vector2f(2, 560));
-	line3.setFillColor(sf::Color::White);
-	line3.setPosition(200, 20);
-
-	sf::RectangleShape line4 = line3;
-	line4.setPosition(400, 20);
-
-	//draw to the window
-	gameWindow.draw(line1);
-	gameWindow.draw(line2);
-	gameWindow.draw(line3);
-	gameWindow.draw(line4);
-}
-
-void MyGame::drawSlot(sf::RenderWindow &gameWindow, int symbol, int x_pos, int y_pos) 
-{
-	if (symbol == 1)      //O
-	{
-		sf::CircleShape shape(60); // use this to change radius !!! setPosition must be also changed if radius was changed !!!
-		shape.setFillColor(sf::Color::Transparent);
-		shape.setOutlineThickness(5);
-		shape.setOutlineColor(sf::Color::White);
-		shape.setPosition(35 + (205 * x_pos), 35 + (205 * y_pos));
-
-		gameWindow.draw(shape);
-	}
-	else if (symbol == 2) //X
-	{
-		//the \ cross
-		sf::RectangleShape cross1(sf::Vector2f(200, 5));
-		cross1.setPosition(25 + (210 * x_pos), 20 + (210 * y_pos));
-		cross1.rotate(45);
-		cross1.setFillColor(sf::Color::White);
-
-		//the / cross
-		sf::RectangleShape cross2(sf::Vector2f(200, 5));
-		cross2.setPosition(25 + (210 * x_pos), 165 + (205 * y_pos));
-		cross2.rotate(-45);
-		cross2.setFillColor(sf::Color::White);
-
-		gameWindow.draw(cross1);
-		gameWindow.draw(cross2);
-	}
-
-}
-
-void MyGame::drawWinScene(sf::RenderWindow &gameWindow, int side)
-{
-	sf::Text winText;
-	sf::Font winFont;
-	if (!winFont.loadFromFile("lucid.ttf"))
-	{
-		std::cerr << ".Error while loading font" << std::endl;
-		exit;
-	} 
-	winText.setFont(winFont);
-	winText.setCharacterSize(80);
-	winText.setColor(sf::Color::Red);
-	winText.setPosition(100, 100);
-
-	if (side == 1) {
-		winText.setString("Plr O wins!");
-		std::cout << "Plr O wins!" << std::endl;
 		
 	}
 	else if (side == 2) {
 		winText.setString("Player X wins!");
-		std::cout << "Plr X wins!" << std::endl;
+	}
+	else if (side == 0) {
+		winText.setString("It's a DRAW!");
 	}
 	gameWindow.draw(winText);	
 }
@@ -479,38 +217,38 @@ bool MyGame::isWinner(std::vector<std::vector<int>> &gameBoard, int side)
 
 }
 
-bool MyGame::handleClick(std::vector<std::vector<int>> &gameBoard, int side, int io, int jo)
+bool MyGame::handleClick(std::vector<std::vector<int>> &gameBoard, int side, int x_axis, int y_axis)
 {
 	int i, j;
 
-	//searching for clicked element and getting is into table array - clicked middle = board[1][1]
+	//searching for clicked element and getting is into table array - e.g. clicked middle = board[1][1]
 
-	//y for i
-	if (io < 200)
+	//checking clicked square - horizontally
+	if (x_axis < 200) 
 	{
-		i = 0;
+		i = 0; // clicked left
 	}
-	else if (io < 400)
+	else if (x_axis < 400)
 	{
-		i = 1;
+		i = 1; // clicked middle
 	}
 	else
 	{
-		i = 2;
+		i = 2; // clicked right
 	}
 
-	//x for j
-	if (jo < 200)
+	//checking clicked square - vertically
+	if (y_axis < 200)
 	{
-		j = 0;
+		j = 0; // clicked top
 	}
-	else if (jo < 400)
+	else if (y_axis < 400)
 	{
-		j = 1;
+		j = 1; // clicked middle
 	}
 	else
 	{
-		j = 2;
+		j = 2; // clicked bottom
 	}
 
 	if (gameBoard[i][j] == 0)   //if the slot is empty
--- Kolko_i_Krzyzyk/MyGame.h
+++ Kolko_i_Krzyzyk/MyGame.h
@@ -2,7 +2,6 @@
 #define MYGAME_H
 
 #include "SFML\Graphics.hpp"
-#include "SFML\Window.hpp"
 #include <vector>
 #include <iostream>
 
@@ -16,32 +15,9 @@ public:
 	void drawSlot(sf::RenderWindow &gameWindow, int symbol, int x_pos, int y_pos);
 	void drawWinScene(sf::RenderWindow &gameWindow, int side);
 	bool isWinner(std::vector<std::vector<int>> &gameBoard, int side);
-	bool handleClick(std::vector<std::vector<int>> &gameBoard, int side, int io, int jo);
-	
-	sf::RenderWindow gameWindow;
-	std::vector<std::vector<int>> gameBoard;
-	
-};
+	bool handleClick(std::vector<std::vector<int>> &gameBoard, int side, int x_axis, int y_axis);
 
-=======
-#ifndef MYGAME_H
-#define MYGAME_H
-
-#include "SFML\Graphics.hpp"
-#include <vector>
-#include <iostream>
-
-class MyGame
-{
-public:
-	MyGame();
-	void play();
-	
-	void drawBoard(sf::RenderWindow &gameWindow);
-	void drawSlot(sf::RenderWindow &gameWindow, int symbol, int x_pos, int y_pos);
-	bool isWinner(std::vector<std::vector<int>> &gameBoard, int side);
-	bool handleClick(std::vector<std::vector<int>> &gameBoard, int side, int io, int jo);
-	
+private:
 	sf::RenderWindow gameWindow;
 	std::vector<std::vector<int>> gameBoard;
 	
